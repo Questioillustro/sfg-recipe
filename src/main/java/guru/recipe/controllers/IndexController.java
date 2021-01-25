5831a6f906e0a2d0f09c243bb09d7196324e45de
Stephen <questioillustro@gmail.com>
2021-01-25 17:11:39 -0500

    Repositories

--- src/main/java/guru/recipe/controllers/IndexController.java
+++ src/main/java/guru/recipe/controllers/IndexController.java
@@ -1,14 +1,34 @@
 package guru.recipe.controllers;
 
+import guru.recipe.domain.Category;
+import guru.recipe.domain.UnitOfMeasure;
+import guru.recipe.repository.CategoryRepository;
+import guru.recipe.repository.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/index"})
     public String getIndexPage() {
-        System.out.println("Some message");
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat Id is: " + categoryOptional.get().getId());
+        System.out.println("UOM Id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
