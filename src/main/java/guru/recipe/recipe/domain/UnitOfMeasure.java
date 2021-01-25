59464f13dd06777931da54fc5642d32ee489c3b3
Stephen <questioillustro@gmail.com>
2021-01-25 12:26:04 -0500

    domain objects, ingredient, uom

---
+++ src/main/java/guru/recipe/recipe/domain/UnitOfMeasure.java
@@ -0,0 +1,31 @@
+package guru.recipe.recipe.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+    private String description;
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
